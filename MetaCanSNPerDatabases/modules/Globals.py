9e659f537d7e825c96429f76ffd707cc4df52ccc
XJ-04561 <107653760+XJ-04561@users.noreply.github.com>
2024-03-28 11:55:22 +0100

    Fix

--- MetaCanSNPerDatabases/modules/Globals.py
+++ MetaCanSNPerDatabases/modules/Globals.py
@@ -4,7 +4,7 @@ from functools import cached_property, cache
 import sqlite3, hashlib, re, os, logging
 from typing import Generator, Callable, Iterable, Self, overload, final, Literal, Any
 
-LOGGER = logging.Logger()
+LOGGER = logging.Logger("MetaCanSNPerDatabases")
 LOGGER.addHandler(logging.FileHandler(os.devnull))
 
 DATABASE_VERSIONS : dict[str,int] = {
