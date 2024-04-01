82d2b0f3e7ae7fd74673937332958ba7ec8d0d4a
XJ-04561 <107653760+XJ-04561@users.noreply.github.com>
2024-04-01 03:20:53 +0200

    Update __main__.py

--- MetaCanSNPerDatabases/__main__.py
+++ MetaCanSNPerDatabases/__main__.py
@@ -119,7 +119,7 @@ def main():
 	elif "--version" in sys.argv:
 		print(f"MetaCanSNPerDatabases v. {CURRENT_VERSION}")
 		exit(0)
-	elif sys.argv[1] not in ["read", "write", "update"]:
+	elif sys.argv[1] not in ["read", "write", "update", "download"]:
 		print("No mode chosen check usage to see which mode is appropriate for your intended use.", file=sys.stderr)
 		parser.print_help()
 		exit(1)
