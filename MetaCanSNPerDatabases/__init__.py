3f330c83f7689116d3b5636bb9de1a8d6a8d8d8f
XJ-04561 <107653760+XJ-04561@users.noreply.github.com>
2024-04-01 22:41:47 +0200

    Removed typeguard

--- MetaCanSNPerDatabases/__init__.py
+++ MetaCanSNPerDatabases/__init__.py
@@ -1,26 +1,15 @@
 
 
-from typeguard import install_import_hook
-
-install_import_hook("MetaCanSNPerDatabases.modules.Globals")
 import MetaCanSNPerDatabases.modules.Globals as Globals
 
-install_import_hook("MetaCanSNPerDatabases.modules.Databases")
 from MetaCanSNPerDatabases.modules.Databases import openDatabase, DatabaseReader, DatabaseWriter, IsLegacyCanSNPer2, OutdatedCanSNPerDatabase
 from sqlite3 import DatabaseError
 
-install_import_hook("MetaCanSNPerDatabases.modules.Columns")
 from MetaCanSNPerDatabases.modules.Columns import ColumnFlag
 import MetaCanSNPerDatabases.modules.Columns as Columns
 
-install_import_hook("MetaCanSNPerDatabases.modules.Functions")
 from MetaCanSNPerDatabases.modules.Functions import downloadDatabase, updateFromLegacy
 
-install_import_hook("MetaCanSNPerDatabases.modules.Tables")
 from MetaCanSNPerDatabases.modules.Tables import SNPTable, ReferenceTable, TreeTable
-
-install_import_hook("MetaCanSNPerDatabases.modules.Tree")
 from MetaCanSNPerDatabases.modules.Tree import Branch
-
-install_import_hook("MetaCanSNPerDatabases.modules.Test")
 import MetaCanSNPerDatabases.modules.Test as Test
