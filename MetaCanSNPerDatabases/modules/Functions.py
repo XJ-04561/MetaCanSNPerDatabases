8a93f92ca8a60b12f351113d279242fef6dae811
XJ-04561 <107653760+XJ-04561@users.noreply.github.com>
2024-04-01 04:01:27 +0200

    Update Functions.py

--- MetaCanSNPerDatabases/modules/Functions.py
+++ MetaCanSNPerDatabases/modules/Functions.py
@@ -88,7 +88,7 @@ def updateFromLegacy(database : DatabaseWriter):
 	database._connection.execute("ALTER TABLE snp_references RENAME TO snp_references_old;")
 	database.ReferenceTable.create()
 	database._connection.execute(f"INSERT INTO {TABLE_NAME_REFERENCES} SELECT * FROM snp_references_old;")
-	database._connection.execute("DROP TABLE snp_reference_old;")
+	database._connection.execute("DROP TABLE snp_references_old;")
 
 	# Chromosomes
 	database.ChromosomesTable.create()
