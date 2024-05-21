c09d132232bfbc651f61e78eea89c6325ec5fb0b
XJ-04561 <107653760+XJ-04561@users.noreply.github.com>
2024-05-22 01:31:36 +0200

    Update Databases.py

--- SQLOOP/_core/Databases.py
+++ SQLOOP/_core/Databases.py
@@ -121,10 +121,10 @@ class Database(metaclass=DatabaseMeta):
 	
 	@ClassProperty
 	def CURRENT_TABLES_HASH(self) -> int:
-		return int.from_bytes(hashlib.md5(whitespacePattern.sub(" ", "; ".join(sorted(map(sql, self.tables)))).encode("utf-8")).hexdigest(), base=16)
+		return int(hashlib.md5(whitespacePattern.sub(" ", "; ".join(sorted(map(sql, self.tables)))).encode("utf-8")).hexdigest(), base=16)
 	@ClassProperty
 	def CURRENT_INDEXES_HASH(self) -> int:
-		return int.from_bytes(hashlib.md5(whitespacePattern.sub(" ", "; ".join(sorted(map(sql, self.indexes)))).encode("utf-8")).hexdigest(), base=16)
+		return int(hashlib.md5(whitespacePattern.sub(" ", "; ".join(sorted(map(sql, self.indexes)))).encode("utf-8")).hexdigest(), base=16)
 
 	LOG = logging.Logger(SOFTWARE_NAME, level=logging.FATAL)
 
@@ -282,12 +282,12 @@ class Database(metaclass=DatabaseMeta):
 	@property
 	def indexesHash(self) -> int:
 		"""hash of the original SQL text that created all indexes in the database."""
-		return int.from_bytes(hashlib.md5(whitespacePattern.sub(" ", "; ".join(sorted(map(lambda s:tableCreationCommand.sub("",s), self(SELECT (SQL) - FROM - SQLITE_MASTER - WHERE (type='index')))))).encode("utf-8")).hexdigest(), base=16)
+		return int(hashlib.md5(whitespacePattern.sub(" ", "; ".join(sorted(map(lambda s:tableCreationCommand.sub("",s), self(SELECT (SQL) - FROM - SQLITE_MASTER - WHERE (type='index')))))).encode("utf-8")).hexdigest(), base=16)
 
 	@property
 	def tablesHash(self) -> int:
 		"""hash of the original SQL text that created all tables in the database."""
-		return int.from_bytes(hashlib.md5(whitespacePattern.sub(" ", "; ".join(sorted(map(lambda s:tableCreationCommand.sub("",s), self(SELECT (SQL) - FROM (SQLITE_MASTER) - WHERE (type='table')))))).encode("utf-8")).hexdigest(), base=16)
+		return int(hashlib.md5(whitespacePattern.sub(" ", "; ".join(sorted(map(lambda s:tableCreationCommand.sub("",s), self(SELECT (SQL) - FROM (SQLITE_MASTER) - WHERE (type='table')))))).encode("utf-8")).hexdigest(), base=16)
 
 	def createIndex(self : Self, index : Index) -> bool:
 		"""Create a given Index-object inside the database. Returns True if succesfull, returns False otherwise."""
